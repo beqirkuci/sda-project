fee079f4302de843da8035dd1d2aaeb40cd9a2ab
Beqir Kuci <beqkuc@private.beeweeb.com>
2021-03-27 21:18:09 +0100

    new task

--- src/main/java/com/sda/orders/orders/controller/HelloWorldController.java
+++ src/main/java/com/sda/orders/orders/controller/HelloWorldController.java
@@ -3,15 +3,21 @@ import com.sda.orders.orders.model.Student;
 import com.sda.orders.orders.services.StudentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Date;
 import java.util.List;
 
 @RestController
 public class HelloWorldController {
 
     private final StudentService studentService;
+
+
+    @Value("${test.value}")
+    private String[] arrayOfStrings;
 
     private static Logger LOG = LoggerFactory
             .getLogger(HelloWorldController.class);
@@ -20,17 +26,32 @@ public class HelloWorldController {
         this.studentService = studentService;
     }
 
-    @GetMapping("student")
-    public List<Student> getStudent() {
+    @GetMapping("students")
+    public ResponseEntity<List<Student>> getStudent() {
 
+        return new ResponseEntity<List<Student>>(studentService.studentList(),
+                HttpStatus.CREATED);
 
-        return studentService.studentList();
 }
 
 
-    @GetMapping("receivePayment")
+
+
+    @GetMapping("student/{email}")
+    /*
+     * Krijoni nje metode ne studentService E cila merr te dhena nga application.properties dhe gjen studentin by Email
+     * Perdorni Student si endpoint
+     * */
     public Integer getMessagge(@RequestParam Integer number1,@RequestParam Integer number2){
         return number1+ number2;
     }
+
+
+
+    /*
+     * Krijoni nje Medote te re POST e cila krijon nje student dhe kthen listen e studenteve qe ndodhen ne sistem
+     * Pra krijo nje student dhe kthe nje array me sudentet qe ndodhen ne Properties file
+     * Perdorni Student si endpoint
+     * */
 
 }
